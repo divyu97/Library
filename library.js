524232d1cb4e603685e612e63b6a5cf86303a8bc
Divyu <divyendureddy@gmail.com>
2024-10-03 20:08:44 +0530

    Create an array for storing books and write a constructor for book objects

--- library.js
+++ library.js
@@ -1,3 +1,20 @@
 const dialog = document.querySelector("dialog");
 
 document.querySelector(".new-book").addEventListener("click", () => dialog.showModal());
+
+const library = [];
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.info = function() {
+        if (status === "READ")
+            return `${this.title} by ${this.author}, ${this.pages} pages, read`;
+        else if (status === "NOT READ")
+            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+        else  
+            return `${this.title} by ${this.author}, ${this.pages} pages, currently reading`;
+    };
+}
