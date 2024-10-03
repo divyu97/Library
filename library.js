8a0c055b97c10dc2c57b411965dc7453ad7b4822
Divyu <divyendureddy@gmail.com>
2024-10-03 20:48:41 +0530

    Write a function for adding new books to the library array and make the "add-book" & "cancel" buttons functional accordingly

--- library.js
+++ library.js
@@ -1,4 +1,8 @@
 const dialog = document.querySelector("dialog");
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let status = document.querySelector("#status");
 
 document.querySelector(".new-book").addEventListener("click", () => dialog.showModal());
 
@@ -18,3 +22,32 @@ function Book(title, author, pages, status) {
             return `${this.title} by ${this.author}, ${this.pages} pages, currently reading`;
     };
 }
+
+function addBookToLibrary() {
+    if (title.value === "")
+        alert("Title field can't be empty");
+    else if (author.value === "")
+        alert("Author field can't be empty");
+    else if (pages.value === "" || Number(pages.value) === 0)
+        alert("Invalid number of pages");
+    else if (status.value === "Select Status")
+        alert("Please select status of the book");
+    else {
+        const book = new Book(title.value, author.value, Number(pages.value), status.value);
+        library.push(book);
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        status.value = "Select Status";
+        dialog.close();
+    }   
+}
+
+document.querySelector(".add-book").addEventListener("click", addBookToLibrary);
+document.querySelector(".cancel").addEventListener("click", () => {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    status.value = "Select Status";
+    dialog.close();
+});
